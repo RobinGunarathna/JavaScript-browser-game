1f2da08bb275b39a5272955c5be4f6b3a2e26af9
RobinGunarathna <madgunrob@gmail.com>
2021-12-21 20:04:09 +0000

    Supposed to se red dot on the website looking for fix

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,5 @@
 // Canvas setup
-const canvas = document.getElementById(canvasOne);
+const canvas = document.getElementById('canvasOne');
 const contxt = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 500;
@@ -10,17 +10,67 @@ contxt.font = '50px Georgia';
 
 // Mouse interactivity
 let canvasPlacement = canvas.getBoundingClientRect();
-console.log(canvasPlacement);
 const mouse = {
     x: canvas.width/2,
     y: canvas.height/2,
     click: false
 }
 canvas.addEventListener('mousedown', function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-
+    mouse.click = true;
+    mouse.x = event.x - canvasPlacement.left;
+    mouse.y = event.y - canvasPlacement.top;
 });
+canvas.addEventListener('mouseup', function(){
+    mouse.click = false;
+})
+
 // Player
+class player {
+    constructor(){
+        this.x = canvas.width/2;
+        this.y = canvas.height/2;
+        this.radius = 50;
+        this.angle = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.frame = 0;
+        this.spriteWidth = 573;
+        this.spriteHeight = 815;
+    }
+    update() {
+        const distanceX = this.x - mouse.x;
+        const distanceY = this.y - mouse.y;
+        if (mouse.x != this.x) {
+            this.x -= distanceX/30;
+        }
+        if (mouse.y != this.y) {
+            this.y -= distanceY/30;
+        }
+    }
+    draw(){
+        if (mouse.click) {
+            contxt.lineWidth = 0.2;
+            contxt.beginPath();
+            contxt.moveTo(this.x, this.y);
+            contxt.lineTo(mouse.x, mouse.y);
+            contxt.stroke();
+        }
+        contxt.fillStyle = 'red';
+        contxt.beginPath();
+        contxt.arc(this.x, this.y, this.radius, 0, math.PI * 2);
+        contxt.fill();
+        contxt.closePath();
+    }
+}
+const player = new player();
+
 // popping bubbles
+
+
 // Animation loop
+function animate(){
+    player.update();
+    player.draw();
+    requestAnimationFrame(animate);
+}
+animate()
